8f9dc6767cedbaab46a32fa6ea0f8a996e85fde7
Suraj Meher <surajmeher886@gmail.com>
2026-02-26 23:53:23 +0530

    Initial commit

--- prime.java
+++ prime.java
@@ -1,3 +1,45 @@
+import java.util.*;
 public class prime {
-    
+    public static boolean isprime(int n){
+        
+    if(n==2){
+        return true;
+    }
+    for(int i=2;i<=Math.sqrt(n);i++){
+        if(n%i==0){
+            return false;
+        }
+        
+    }
+    return true;
+}
+public static void rangeinprime(int n){
+int cont=1;
+        for(int i = 2;i<=n;i++){
+            if (isprime(i)==true) {
+                System.out.println(cont+".it is prime number "+i);
+                cont++;
+            }else{ 
+                continue;
+            }
+        }
+}
+public static boolean twinprimeno(int n){
+
+    if(isprime(n)==true&isprime(n+2)==true){
+        return true;
+    }
+    return false;
+}
+
+    public static void main(String[] args) {
+        int a=100;
+        for(int i=2;i<=a;i++){
+            if(twinprimeno(i)==true){
+
+                System.out.println("This are twin prime ("+i+","+(i+2)+")");
+            }
+        }
+        
+    }
 }
--- test.java
+++ test.java
@@ -34,10 +34,13 @@ public class test {
         return true;
         
     }
+    public static int bintodec (int n){
+        
+    }
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         System.out.println("enter the number ");
         int a=sc.nextInt();
-        
+
 }
 }
